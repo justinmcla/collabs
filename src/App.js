17f908e3a0153e1afd942b065bbd42c134c1a449
Justin McLawhorn <jwmclawhorn@gmail.com>
2021-02-03 22:09:07 -0500

    switch to functional component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
+const App = () => (
   <Router>
     <div className="App">
       <Route exact path="/" render={() => <Splash />} />
       <Route exact path="/home" render={() => <Home />} />
     </div>
   </Router>
+)
 
 export default App;
