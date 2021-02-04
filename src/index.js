80f13b0b6a70a2032cf5f0451d2450fadf9ce4ae
Justin McLawhorn <jwmclawhorn@gmail.com>
2021-02-03 22:04:48 -0500

    add auth0 provider

--- src/index.js
+++ src/index.js
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Auth0Provider } from '@auth0/auth0-react'
 
 ReactDOM.render(
+  <Auth0Provider
+    domain="dev--db8unc5.us.auth0.com"
+    clientId="EsyBndC2rhQKoQmnSmOe8T1syJVvkb2v"
+    redirectUri={'http://localhost:3000/home'}
+  >
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+    </React.StrictMode>
+  </Auth0Provider>,
   document.getElementById('root')
 );
 
