2c2296f4f7daa30b30ad4169278d5eb0b453fa14
Justin McLawhorn <jwmclawhorn@gmail.com>
2021-02-03 22:09:26 -0500

    import container components for router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Splash from './containers/Splash'
+import Home from './containers/Home'
 
 const App = () => (
   <Router>
