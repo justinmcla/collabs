d4d0a51175ae4265a1bac2971420a26463ac168a
Justin McLawhorn <jwmclawhorn@gmail.com>
2021-02-04 20:49:52 -0500

    deletes code from git demonstration

--- src/App.js
+++ src/App.js
@@ -3,13 +3,9 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Splash from './containers/Splash'
 import Home from './containers/Home'
 
-// Altered code
-
-// Another comment
-
 const App = () => (
   <Router>
-    <div className="App">
+    <div className="App h-100">
       <Route exact path="/" render={() => <Splash />} />
       <Route exact path="/home" render={() => <Home />} />
     </div>
