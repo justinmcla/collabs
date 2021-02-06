022ecc1b3534e6cbdec99c0da65c310c65c5d5ad
Justin McLawhorn <jwmclawhorn@gmail.com>
2021-02-06 16:45:28 -0500

    adds protected route wrapper to home route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
+import ProtectedRoute from './auth/ProtectedRoute'
 import Splash from './containers/Splash'
 import Home from './containers/Home'
 
 const App = () => (
   <Router>
-    <div className="App h-100">
-      <Route exact path="/" render={() => <Splash />} />
-      <Route exact path="/home" render={() => <Home />} />
+    <div className="App">
+      <Route exact path="/" component={Splash} />
+      <ProtectedRoute exact path="/home" component={Home} />
     </div>
   </Router>
 )
