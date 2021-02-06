f230b0f8647e805a1c44e7d27150a715f804caf5
Justin McLawhorn <jwmclawhorn@gmail.com>
2021-02-05 21:57:50 -0500

    adds splash title stylings, fixes whitespace

--- src/components/SplashTitle.js
+++ src/components/SplashTitle.js
@@ -1,7 +1,16 @@
 import React from 'react'
 
 const SplashTitle = () => {
-  return <div><h1>collabs</h1><br /><h2>a social network for creatives</h2></div>
+  return (
+    <div className="text-right mr-6 pr-6 border-r-2 border-black dark:border-white hover:cursor-default">
+      <h1 className="text-6xl font-header font-extrabold text-gradient themed-gradient hover:animate-gradient-xy">
+        collabs
+      </h1>
+      <h2 className="text-3xl font-header font-medium dark:text-white">
+        a social network for creatives
+      </h2>
+    </div>
+  )
 }
 
 export default SplashTitle
