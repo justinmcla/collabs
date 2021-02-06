0ff53634a01f7328163af20034f920d66e0a1a52
Justin McLawhorn <jwmclawhorn@gmail.com>
2021-02-05 21:54:04 -0500

    turns on dark mode and sets to user preferences

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   purge: ['./src/**/*.js', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'media', // or 'media' or 'class'
   theme: {
     extend: {},
   },
