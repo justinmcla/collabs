33a1cbae17c7c6e665907b6d71b30e4b762ecfd1
Justin McLawhorn <jwmclawhorn@gmail.com>
2021-02-05 21:58:24 -0500

    adds styling, adds splash title component

--- src/containers/Splash.js
+++ src/containers/Splash.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import SplashTitle from '../components/SplashTitle'
 import UniversalLogin from '../components/UniversalLogin'
 
 const Splash = () => {
-  return <div><UniversalLogin /></div>
+  return (
+    <div className="bg-gray-200 dark:bg-gray-800 h-screen flex flex-row flex-wrap justify-center items-center">
+      <SplashTitle/>
+      <UniversalLogin />
+    </div>
+  )
 }
 
 export default Splash
