f8bb540d4c5c075d80589c995c034f882e754268
Justin McLawhorn <jwmclawhorn@gmail.com>
2021-02-05 21:58:04 -0500

    adds tailwind stylings

--- src/components/UniversalLogin.js
+++ src/components/UniversalLogin.js
@@ -3,7 +3,13 @@ import { useAuth0 } from '@auth0/auth0-react'
 
 const UniversalLogin = () => {
   const { loginWithRedirect } = useAuth0()
-  return <button onClick={() => loginWithRedirect()}>UNIVERSAL LOGIN</button>
+  return (
+    <button 
+      className="p-2 border-4 themed-gradient text-white focus:outline-none transform focus:translate-y-0.5 border-white w-max whitespace-nowrap font-body font-bold hover:animated-gradient shadow-lg"
+      onClick={() => loginWithRedirect()}>
+      UNIVERSAL LOGIN
+    </button>
+  )
 }
 
 export default UniversalLogin
