46361f6b2b59c909b44e99ff0d6199fa70117950
justinmcla <jwmclawhorn@gmail.com>
2021-02-17 22:55:58 -0500

    Adds react-redux provider & store

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css'
+import './index.css';
 import App from './App';
-import { Auth0Provider } from '@auth0/auth0-react'
+import { Auth0Provider } from '@auth0/auth0-react';
+import { Provider } from 'react-redux';
+import { store } from './store';
 
 ReactDOM.render(
-  <Auth0Provider
-    domain="dev--db8unc5.us.auth0.com"
-    clientId="EsyBndC2rhQKoQmnSmOe8T1syJVvkb2v"
-    redirectUri={'http://localhost:3000/home'}
-  >
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Auth0Provider>,
+  <Provider store={store}>
+    <Auth0Provider
+      domain="dev--db8unc5.us.auth0.com"
+      clientId="EsyBndC2rhQKoQmnSmOe8T1syJVvkb2v"
+      redirectUri='http://localhost:3000/home'
+    >
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Auth0Provider>
+  </Provider>,
   document.getElementById('root')
 );
