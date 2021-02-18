f904e70601cd27bffb531a383b672abf44bbf980
justinmcla <jwmclawhorn@gmail.com>
2021-02-17 22:55:27 -0500

    Creates Navbar component

---
+++ src/components/profile/Navbar.js
@@ -0,0 +1,7 @@
+const Navbar = () => (
+  <div className="h-screen themed-gradient w-64">
+    <h1 className="font-header font-extrabold text-white text-5xl text-center m-6">collabs</h1>
+  </div>
+)
+
+export default Navbar
