520f8cc8510cfcd7310eb26911fa16fde4419f29
justinmcla <jwmclawhorn@gmail.com>
2021-02-20 17:14:37 -0500

    Removes Logout button, adds basic styling

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,16 +1,16 @@
 import React from 'react'
-import UniversalLogout from '../components/UniversalLogout'
-import { useAuth0 } from '@auth0/auth0-react'
+import Navbar from '../components/profile/Navbar'
 
+import { useAuth0 } from '@auth0/auth0-react'
 const Home = () => {
   const { user, isAuthenticated } = useAuth0();
   return (
     isAuthenticated && (
-      <div>
-        <UniversalLogout />
-        <img src={user.picture} alt={user.name} />
-        <h2>{user.name}</h2>
-        <p>{user.email}</p>
+      <div className="flex flex-row flex-nowrap bg-gray-200 dark:bg-gray-800">
+        <Navbar />
+        <div className="p-8">
+          <h2 className="text-2xl text-white font-body">{user.name}</h2>
+        </div>
       </div>
     )
   )
