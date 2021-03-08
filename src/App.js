493f394eea7a999f836660c546eeef9d2157e929
Justin McLawhorn <jwmclawhorn@gmail.com>
2021-03-07 22:53:33 -0500

    creates new protected routes for new components

--- src/App.js
+++ src/App.js
@@ -3,12 +3,20 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 import ProtectedRoute from './auth/ProtectedRoute'
 import Splash from './containers/Splash'
 import Home from './containers/Home'
+import Collabs from './containers/Collabs'
+import Portfolios from './containers/Portfolios'
+import Callboard from './containers/Callboard'
+import Settings from './containers/Settings'
 
 const App = () => (
   <Router>
     <div className="App">
-      <Route exact path="/" component={Splash} />
-      <ProtectedRoute exact path="/home" component={Home} />
+      <Route exact path="/" component={ Splash } />
+      <ProtectedRoute exact path="/home" component={ Home } />
+      <ProtectedRoute exact path="/collabs" component={ Collabs } />
+      <ProtectedRoute exact path="/portfolios" component={ Portfolios } />
+      <ProtectedRoute exact path="/callboard" component={ Callboard } />
+      <ProtectedRoute exact path="/settings" component={ Settings } />
     </div>
   </Router>
 )
