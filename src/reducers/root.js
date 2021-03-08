e6a2f47ca37492d6895fa52f533d4450fe4dd50a
Justin McLawhorn <jwmclawhorn@gmail.com>
2021-03-07 22:49:39 -0500

    import new reducers to combined reducer

--- src/reducers/root.js
+++ src/reducers/root.js
@@ -1,5 +1,14 @@
-import { combineReducers } from 'redux';
+import { combineReducers } from 'redux'
+import profileReducer from './profile.js'
+import collabReducer from './collab.js'
+import callboardReducer from './callboard.js'
+import portfolioReducer from './portfolio.js'
 
-const rootReducer = () => { };
+const rootReducer = combineReducers({
+  profile: profileReducer,
+  collab: collabReducer,
+  callboard: callboardReducer,
+  portfolio: portfolioReducer
+})
 
 export default rootReducer;
