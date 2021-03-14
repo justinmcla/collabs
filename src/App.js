4cddf700a76d544a6df7601e617019bfccd99416
Justin McLawhorn <jwmclawhorn@gmail.com>
2021-03-14 13:00:19 -0400

    Moves Navbar out of individual components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
 import ProtectedRoute from './auth/ProtectedRoute'
 import Splash from './containers/Splash'
 import Home from './containers/Home'
@@ -8,15 +9,20 @@ import Portfolios from './containers/Portfolios'
 import Callboard from './containers/Callboard'
 import Settings from './containers/Settings'
 
+const isProfilePath = window.location.pathname !== "/"
+
 const App = () => (
   <Router>
-    <div className="App">
+    <div className="App h-full">
       <Route exact path="/" component={ Splash } />
-      <ProtectedRoute exact path="/home" component={ Home } />
-      <ProtectedRoute exact path="/collabs" component={ Collabs } />
-      <ProtectedRoute exact path="/portfolios" component={ Portfolios } />
-      <ProtectedRoute exact path="/callboard" component={ Callboard } />
-      <ProtectedRoute exact path="/settings" component={ Settings } />
+      <div className="flex flex-row flex-nowrap">
+        { isProfilePath ? <Navbar /> : null }
+        <ProtectedRoute exact path="/home" component={ Home } />
+        <ProtectedRoute exact path="/collabs" component={ Collabs } />
+        <ProtectedRoute exact path="/portfolios" component={ Portfolios } />
+        <ProtectedRoute exact path="/callboard" component={ Callboard } />
+        <ProtectedRoute exact path="/settings" component={ Settings } />
+      </div>
     </div>
   </Router>
 )
