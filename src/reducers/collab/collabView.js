f96470133a7376a7e54a5bebfdd92abec5811ad5
Justin McLawhorn <jwmclawhorn@gmail.com>
2021-03-18 09:12:22 -0400

    adds clear collab view action case

--- src/reducers/collab/collabView.js
+++ src/reducers/collab/collabView.js
@@ -6,6 +6,8 @@ const collabViewReducer = (state = { collab: {}, isLoading: false, errors: [] },
       return { ...state, isLoading: false, errors: action.errors }
     case "READ_COLLAB_VIEW_SUCCESS":
       return { ...state, collab: action.collab, isLoading: false }
+    case "CLEAR_COLLAB_VIEW":
+      return { ...state, collab: {}, isLoading: false }
     default:
       return state
   }
