ec0b5eb3dc27627b1a5aa551475890fba29e0594
Justin McLawhorn <jwmclawhorn@gmail.com>
2021-03-18 09:14:51 -0400

    updates how readName action fetches data

--- src/actions/profile/name/readName.js
+++ src/actions/profile/name/readName.js
@@ -1,21 +1,19 @@
-const readName = accessToken => {
+const readName = (accessToken) => {
   return async dispatch => {
-    dispatch({
-      type: "NEW_NAME_REQUEST"
-    })
+    dispatch({ type: "NEW_NAME_REQUEST" });
+    const endpoint = window.localStorage.getItem('endpoint')
     try {
-      const response = await fetch("", {
+      const response = await fetch(`${process.env.REACT_APP_BASE_API}/profiles/${endpoint}`, {
         headers: {
           Authorization: `Bearer ${accessToken}`
         }
       })
-      dispatch({
-        type: "READ_NAME_SUCCESS",
-        name: response.json().name
-      })
+      const json = await response.json()
+      console.log(json)
+      dispatch({ type: "READ_NAME_SUCCESS", name: json.data.attributes.name })
     } catch (errors) {
       dispatch({
-        type: "READ_NAME_FAILURE",
+        type: "NAME_REQUEST_FAILURE",
         errors: errors
       })
     }
