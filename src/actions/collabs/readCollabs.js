ba62a858e712c31ef56cf722e09276f573aea6ff
Justin McLawhorn <jwmclawhorn@gmail.com>
2021-03-18 09:13:29 -0400

    builds action to read collab list

---
+++ src/actions/collabs/readCollabs.js
@@ -0,0 +1,27 @@
+const readCollabs = accessToken => {
+  return async dispatch => {
+    dispatch({
+      type: "NEW_COLLAB_LIST_REQUEST"
+    })
+    try {
+      const endpoint = window.localStorage.getItem('endpoint')
+      const response = await fetch(`${process.env.REACT_APP_BASE_API}/collaborations/${endpoint}`, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`
+        }
+      })
+      const data = await response.json()
+      dispatch({
+        type: "READ_COLLAB_LIST_SUCCESS",
+        collabs: data
+      })
+    } catch (errors) {
+      dispatch({
+        type: "COLLAB_LIST_REQUEST_FAILURE",
+        errors: errors
+      })
+    }
+  }
+}
+
+export default readCollabs
