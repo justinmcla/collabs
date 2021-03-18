56cfc286298f25adba945f546403f46dfda4e28d
Justin McLawhorn <jwmclawhorn@gmail.com>
2021-03-18 09:12:12 -0400

    adds cases to handle collab queries

--- src/reducers/collab/collabSearch.js
+++ src/reducers/collab/collabSearch.js
@@ -6,6 +6,12 @@ const collabSearchReducer = (state = { results: [], isLoading: false, errors: []
       return { ...state, isLoading: false, errors: action.errors }
     case "READ_COLLAB_SEARCH_SUCCESS":
       return { ...state, results: action.collabs, isLoading: false }
+    case "NEW_COLLAB_QUERY_REQUEST":
+      return { ...state, isLoading: true }
+    case "COLLAB_QUERY_REQUEST_FAILURE":
+      return { ...state, isLoading: false, errors: action.errors }
+    case "COLLAB_QUERY_REQUEST_SUCCESS":
+      return { ...state, results: action.collabs, isLoading: false }
     default:
       return state
   }
