cebbbac517a8be1c87f9a25e07e9d626556ec5b5
Justin McLawhorn <jwmclawhorn@gmail.com>
2021-03-18 09:01:35 -0400

    removes create/delete, updates to single location

--- src/reducers/profile/location.js
+++ src/reducers/profile/location.js
@@ -1,18 +1,15 @@
-const locationReducer = (state = { list: [], isLoading: false, errors: [] }, action) => {
+const locationReducer = (state = { address: {}, isLoading: false, errors: [] }, action) => {
   switch(action.type) {
     case "NEW_LOCATION_REQUEST":
       return { ...state, isLoading: true }
     case "LOCATION_REQUEST_FAILURE":
       return { ...state, isLoading: false, errors: action.errors }
-    case "CREATE_LOCATION_SUCCESS":
-      return { ...state, list: [...state.locations, action.location], isLoading: false }
-    case "READ_LOCATIONS_SUCCESS":
-      return { ...state, list: action.locations, isLoading: false }
+    case "READ_PROFILE_SUCCESS":
+      return { ...state, address: action.profile.location, isLoading: false }
+    case "READ_LOCATION_SUCCESS":
+      return { ...state, address: action.location, isLoading: false }
     case "UPDATE_LOCATIONS_SUCCESS":
-      return { ...state, list: action.locations, isLoading: false }
-    case "DELETE_LOCATION_SUCCESS":
-      const locations = state.locations.filter(location => location.id !== action.id)
-      return { ...state, list: locations, isLoading: false }
+      return { ...state, address: action.location, isLoading: false }
     default:
       return state
   }
