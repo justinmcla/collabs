51d2c8ed174dbf610aa1a7171038fb99be4c9b82
Justin McLawhorn <jwmclawhorn@gmail.com>
2021-03-18 09:02:03 -0400

    removes create/delete, updates to single job

--- src/reducers/profile/job.js
+++ src/reducers/profile/job.js
@@ -1,18 +1,15 @@
-const jobReducer = (state = { list: [], isLoading: false, errors: [] }, action) => {
+const jobReducer = (state = { title: '', isLoading: false, errors: [] }, action) => {
   switch(action.type) {
     case "NEW_JOB_REQUEST":
       return {...state, isLoading: true }
     case "JOB_REQUEST_FAILURE":
       return {...state, isLoading: false, errors: action.errors }
-    case "CREATE_JOB_SUCCESS":
-      return {...state, list: [...state.jobs, action.job], isLoading: false }
-    case "READ_JOBS_SUCCESS":
-      return {...state, list: action.jobs, isLoading: false }
+    case "READ_PROFILE_SUCCESS":
+      return { ...state, title: action.profile.profession, isLoading: false }
+    case "READ_JOB_SUCCESS":
+      return {...state, title: action.job, isLoading: false }
     case "UPDATE_JOB_SUCCESS":
-      return {...state, list: action.jobs, isLoading: false }
-    case "DELETE_JOB_SUCCESS":
-      const jobs = state.jobs.filter(job => job.id !== action.id)
-      return {...state, list: jobs, isLoading: false }
+      return {...state, title: action.job, isLoading: false }
     default:
       return state
   }
