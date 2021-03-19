9e344f2796381ac05ee4764770db8077f3e8e302
Justin McLawhorn <jwmclawhorn@gmail.com>
2021-03-19 19:06:45 -0400

    adds profile photo form, wraps rows in tbody

--- src/containers/profile/PublicProfileSettings.js
+++ src/containers/profile/PublicProfileSettings.js
@@ -1,4 +1,5 @@
 import { useSelector } from 'react-redux'
+import ProfilePhotoForm from '../../components/profile/ProfilePhotoForm'
 import ProfileProperty from './ProfileProperty'
 
 const PublicProfileSettings = () => {
@@ -8,13 +9,16 @@ const PublicProfileSettings = () => {
       <div className="p-8">
         <h1 className="text-4xl text-gray-700 dark:text-gray-300 font-body italic cursor-default">Edit your public profile settings</h1>
         <table className="m-4 text-gray-700 dark:text-gray-300">
-          <ProfileProperty column="Name" value={ name.name } />
-          <ProfileProperty column="Profession" value={ job.title } />
-          <ProfileProperty column="Location" value={ `${location.address.city}, ${location.address.state} ${location.address.zip}` } />
-          <ProfileProperty column="Public URL" value={ url.path } />
-          <tr><td colspan="2"><br />Social Links</td></tr>
-          { socialLink.list.map( link => <ProfileProperty column={ link.provider } value={ link.handle } />)}
+          <tbody>
+            <ProfileProperty column="Name" value={ name.name } />
+            <ProfileProperty column="Profession" value={ job.title } />
+            <ProfileProperty column="Location" value={ `${location.address.city}, ${location.address.state} ${location.address.zip}` } />
+            <ProfileProperty column="Public URL" value={ url.path } />
+            <tr><td colSpan="2"><br />Social Links</td></tr>
+            { socialLink.list.map( link => <ProfileProperty column={ link.provider } value={ link.handle } key={ link.id } />)}
+          </tbody>
         </table>
+        <ProfilePhotoForm />
       </div>
     </div>
   )
