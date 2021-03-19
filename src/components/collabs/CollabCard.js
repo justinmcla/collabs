c9ee537e705598ed77151f28db5ba0b06933d288
Justin McLawhorn <jwmclawhorn@gmail.com>
2021-03-19 18:53:00 -0400

    removes link props, adds flex div to card

--- src/components/collabs/CollabCard.js
+++ src/components/collabs/CollabCard.js
@@ -1,21 +1,20 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import CollabStatusButton from './CollabStatusButton'
 
-const CollabCard = ({ profile }) => {
+const CollabCard = ({ profile, type }) => {
   return (
-  <Link to={{
-    pathname: `/profile${profile.url}`,
-    state: {
-      endpoint: 'test'
-    }
-  }}>
-    <div className="bg-gray-200 dark:bg-gray-800 shadow-md m-6 flex flex-row flex-nowrap p-4 w-auto items-center transition duration-500 ease-in-out transform hover:-translate-y-2 cursor-pointer">
-      <img src={ profile.image || '/default_profile_photo.png' } alt={ profile.name } className="w-16 border-2 border-white rounded-full mr-2 shadow-lg" />
-      <div className="dark:text-white">
-        <h1 className="font-bold text-thPurple dark:text-thMagenta">{ profile.name }</h1>
-        <h2>{ profile.profession }</h2>
-        <h2>{ `${profile.location.city}, ${profile.location.state}` }</h2>
+  <Link to={`/profile${profile.url}`}>
+    <div className="bg-gray-200 dark:bg-gray-800 shadow-md m-6 flex flex-col flex-nowrap p-4 w-auto items-center transition duration-500 ease-in-out transform hover:-translate-y-2 cursor-pointer">
+      <div className="flex flex-row self-start">
+        <img src={ profile.image || '/default_profile_photo.png' } alt={ profile.name } className="w-16 border-2 border-white rounded-full mr-2 shadow-lg" />
+        <div className="dark:text-white">
+          <h1 className="font-bold text-thPurple dark:text-thMagenta">{ profile.name }</h1>
+          <h2>{ profile.profession }</h2>
+          <h2>{ `${profile.location.city}, ${profile.location.state}` }</h2>
+        </div>
       </div>
+      <CollabStatusButton user={ profile.uuid } type={ type }/>
     </div>
   </Link>
   )
