19bb7b66426a74e1e74e6f875465fbe4f9cc6362
Justin McLawhorn <jwmclawhorn@gmail.com>
2021-03-19 18:50:50 -0400

    adds protected routes for profile, requests

--- src/App.js
+++ src/App.js
@@ -9,7 +9,9 @@ import Portfolios from './containers/Portfolios'
 import Callboard from './containers/Callboard'
 import PublicProfileSettings from './containers/profile/PublicProfileSettings'
 import CollabSearch from './containers/collab/CollabSearch'
+import CollabRequests from './containers/collab/CollabRequests'
 import CollabProfile from './containers/profile/CollabProfile'
+import Profile from './containers/profile/Profile'
 
 const isProfilePath = window.location.pathname !== "/"
 
@@ -20,12 +22,14 @@ const App = () => (
       <div className="flex flex-row flex-nowrap">
         { isProfilePath ? <Navbar /> : null }
         <ProtectedRoute exact path="/home" component={ Home } />
+        <ProtectedRoute exact path="/profile" component={ Profile } />
+        <ProtectedRoute path="/profile/:slug" component={ CollabProfile } />
         <ProtectedRoute exact path="/collabs" component={ Collabs } />
         <ProtectedRoute exact path="/collabs/search" component={ CollabSearch } />
+        <ProtectedRoute exact path="/collabs/requests" component={ CollabRequests } />
         <ProtectedRoute exact path="/portfolios" component={ Portfolios } />
         <ProtectedRoute exact path="/callboard" component={ Callboard } />
         <ProtectedRoute exact path="/settings" component={ PublicProfileSettings } />
-        <ProtectedRoute path="/profile/:slug" component={ CollabProfile } />
       </div>
     </div>
   </Router>
