be9e93a6c5b82f3c92d27956126f1bb8f85ad782
Justin McLawhorn <jwmclawhorn@gmail.com>
2021-03-19 19:02:22 -0400

    deletes post history reducer

--- src/reducers/callboard.js
+++ src/reducers/callboard.js
@@ -1,12 +1,10 @@
 import { combineReducers } from 'redux'
 import postReducer from './callboard/post'
-import postHistoryReducer from './callboard/postHistory'
 import searchReducer from './callboard/postSearch'
 import settingReducer from './callboard/callboardSetting'
 
 const callboardReducer = combineReducers({
   post: postReducer,
-  postHistory: postHistoryReducer,
   search: searchReducer,
   setting: settingReducer
 })
