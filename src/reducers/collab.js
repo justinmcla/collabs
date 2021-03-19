56edcb65ebe5dafdf27b76fe4c59a8d214b61c37
Justin McLawhorn <jwmclawhorn@gmail.com>
2021-03-19 19:02:05 -0400

    deletes request history reducer

--- src/reducers/collab.js
+++ src/reducers/collab.js
@@ -1,14 +1,12 @@
 import { combineReducers } from 'redux'
 import collabListReducer from './collab/collabList'
 import collabRequestReducer from './collab/collabRequest'
-import requestHistoryReducer from './collab/requestHistory'
 import viewReducer from './collab/collabView'
 import searchReducer from './collab/collabSearch'
 
 const collabReducer = combineReducers({
   collabList: collabListReducer,
   collabRequest: collabRequestReducer,
-  requestHistory: requestHistoryReducer,
   view: viewReducer,
   search: searchReducer
 })
