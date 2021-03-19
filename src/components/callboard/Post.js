d1071b06f026a8eb4837c9fa27695a7c00368e99
Justin McLawhorn <jwmclawhorn@gmail.com>
2021-03-19 19:05:26 -0400

    Builds post component

---
+++ src/components/callboard/Post.js
@@ -0,0 +1,14 @@
+import Address from '../profile/Address'
+import { Link } from 'react-router-dom'
+
+const Post = ({ post }) => {
+  return (
+    <div className="bg-gray-200 dark:bg-gray-800 shadow-md m-6 p-4 w-auto transition duration-500 ease-in-out transform hover:-translate-y-2 max-w-sm">
+      <h2 className="text-lg font-bold">{ post.title }</h2>
+      <small><Link to={`/profile${ post.author.url }`}>{ post.author.name }</Link>, <Address {...post.author.location } /></small>
+      <p className="text-justify mt-2">{ post.content }</p>
+    </div>
+  )
+}
+
+export default Post
