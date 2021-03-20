5e8940bd2615d749f2d0785504ad04ed07e3a5bd
Justin McLawhorn <jwmclawhorn@gmail.com>
2021-03-20 09:18:40 -0400

    adds hover effect to social icons

--- src/components/profile/SocialIcon.js
+++ src/components/profile/SocialIcon.js
@@ -9,19 +9,19 @@ import { ReactComponent as Youtube } from '../../assets/Youtube.svg'
 const SocialIcon = ({ provider, handle }) => {
   switch(provider) {
     case "Facebook":
-      return <a href={`https://facebook.com/${ handle }`} className="p-1"><Facebook /></a>
+      return <a href={`https://facebook.com/${ handle }`} className="p-1 transform hover:text-thMagenta duration-300 ease-in-out"><Facebook /></a>
     case "Github":
-      return <a href={`https://github.com/${ handle }`} className="p-1"><Github /></a>
+      return <a href={`https://github.com/${ handle }`} className="p-1 transform hover:text-thMagenta duration-300 ease-in-out"><Github /></a>
     case "Instagram":
-      return <a href={`https://instagram.com/${ handle }`} className="p-1"><Instagram /></a>
+      return <a href={`https://instagram.com/${ handle }`} className="p-1 transform hover:text-thMagenta duration-300 ease-in-out"><Instagram /></a>
     case "LinkedIn":
-      return <a href={`https://linkedin.com/${ handle }`} className="p-1"><LinkedIn /></a>
+      return <a href={`https://linkedin.com/${ handle }`} className="p-1 transform hover:text-thMagenta duration-300 ease-in-out"><LinkedIn /></a>
     case "Medium":
-      return <a href={`https://medium.com/${ handle }`} className="p-1"><Medium /></a>
+      return <a href={`https://medium.com/${ handle }`} className="p-1 transform hover:text-thMagenta duration-300 ease-in-out"><Medium /></a>
     case "Twitter":
-      return <a href={`https://twitter.com/${ handle }`} className="p-1"><Twitter /></a>
+      return <a href={`https://twitter.com/${ handle }`} className="p-1 transform hover:text-thMagenta duration-300 ease-in-out"><Twitter /></a>
     case "Youtube":
-      return <a href={`https://youtube.com/${ handle }`} className="p-1"><Youtube /></a>
+      return <a href={`https://youtube.com/${ handle }`} className="p-1 transform hover:text-thMagenta duration-300 ease-in-out"><Youtube /></a>
     default:
       return null
   }
