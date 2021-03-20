9d07eb2d19794c88f57d4524e00e080fda1b6040
Justin McLawhorn <jwmclawhorn@gmail.com>
2021-03-20 09:19:36 -0400

    changes verbiage from search to query

--- src/containers/collab/CollabSearch.js
+++ src/containers/collab/CollabSearch.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react'
 import CollabCard from '../../components/collabs/CollabCard'
 import { useSelector, useDispatch } from 'react-redux'
 import { useAuth0 } from '@auth0/auth0-react'
-import searchCollabs from '../../actions/collabs/searchCollabs'
+import queryCollabs from '../../actions/collabs/queryCollabs'
 import CollabSearchForm from '../../components/collabs/CollabSearchForm'
 
 const CollabSearch = () => {
@@ -13,17 +13,17 @@ const CollabSearch = () => {
   useEffect(() => {
     (async () => {
       const token = await getAccessTokenSilently()
-      dispatch(searchCollabs(token))
+      dispatch(queryCollabs(token))
     })()
   }, [dispatch, getAccessTokenSilently])
 
   return (
-    <div className="h-screen w-full flex flex-row flex-nowrap bg-gray-200 dark:bg-gray-800">
+    <div className="h-screen w-full flex flex-row flex-nowrap bg-gray-200 dark:bg-gray-800 overflow-auto">
       <div className="p-8">
         <h1 className="text-4xl text-gray-700 dark:text-gray-300 font-body italic cursor-default">Find your next collaborator.</h1>
         <br />
         <CollabSearchForm />
-        { collabs.map(collab => <CollabCard profile={ collab } key={ collab.uuid } />) }
+        { collabs.map(collab => <CollabCard profile={ collab } key={ collab.uuid } type="search" />) }
       </div>
     </div>
   )
