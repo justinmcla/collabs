caaa4b4fa549bbff9d20075ec86c7988e90bac48
Justin McLawhorn <jwmclawhorn@gmail.com>
2021-03-21 15:09:47 -0400

    remove loading spinner

--- src/components/profile/ProfilePhotoForm.js
+++ src/components/profile/ProfilePhotoForm.js
@@ -1,15 +1,13 @@
 import { useState, useCallback } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux'
 import { useAuth0 } from '@auth0/auth0-react'
 import { ReactComponent as Check } from '../../assets/Check.svg'
-import { ReactComponent as LoadingSpinner } from '../../assets/LoadingSpinner.svg'
 import updateImage from '../../actions/profile/updateImage'
 
 const ProfilePhotoForm = () => {
   const [fileLoaded, setFileLoaded] = useState(0)
   const dispatch = useDispatch()
   const { getAccessTokenSilently } = useAuth0()
-  const isLoading = useSelector(state => state.profile.isLoading)
 
   const handleSubmit = useCallback(
     async event => {
@@ -22,22 +20,16 @@ const ProfilePhotoForm = () => {
     }, [dispatch, getAccessTokenSilently]
   )
 
-  if (isLoading) {
-    return (
-      <span><LoadingSpinner className="w-48" /></span>
-    )
-  } else {
-    return (
-    <div>
-      <form className="flex flex-row items-center" onSubmit={ handleSubmit } id="imageUpload">
-        <label htmlFor="fileInput" className="p-3 bg-gray-100 dark:bg-gray-700 cursor-pointer transform hover:shadow-lg hover:bg-green-200 ease-in-out duration-300">Change Profile Image</label>
+  return (
+    <div className="mx-8 w-full -mt-8 pr-12">
+      <form className="flex flex-row items-center justify-between" onSubmit={ handleSubmit } id="imageUpload">
+        <label htmlFor="fileInput" className="p-3 flex-grow bg-gray-100 dark:bg-gray-700 cursor-pointer transform hover:bg-blue-200 ease-in-out duration-300">Change Profile Image</label>
         <input type="file" id="fileInput" name="fileInput" className="hidden" onInput={ () => setFileLoaded(1) } />
         <span>{ fileLoaded === 1 ? <Check className="w-4 ml-4 fill-current text-green-500"/> : null }</span>
-        <input type="submit" value="Upload" className="m-4 p-3 bg-gray-100 dark:bg-gray-700 cursor-pointer hover:bg-green-200 ease-in-out duration-300"/>
+        <input type="submit" value="Upload" className="m-4 p-3 bg-gray-100 dark:bg-gray-700 cursor-pointer hover:bg-blue-200 ease-in-out duration-300"/>
       </form>
     </div>
-    )
-  }
+  )
 }
 
 export default ProfilePhotoForm
