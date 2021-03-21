00439daa3e104ba3cfc6e8bd8243b3fac78a5d85
Justin McLawhorn <jwmclawhorn@gmail.com>
2021-03-21 12:10:11 -0400

    builds action to search for new collaborators

---
+++ src/actions/collabs/collabSearch/queryCollabs.js
@@ -0,0 +1,22 @@
+const queryCollabs = (accessToken, query = 'all') => {
+  return async dispatch => {
+    dispatch({ type: "NEW_COLLAB_QUERY_REQUEST" })
+    try {
+      const response = await fetch(`${process.env.REACT_APP_BASE_API}/profiles?query=${query}`, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`
+        }
+      })
+      const json = await response.json()
+      if (json.status === 200) {
+        dispatch({ type: "COLLAB_QUERY_REQUEST_SUCCESS", collabs: json.data })
+      } else {
+        throw new Error('Unable to fetch collabs')
+      }
+    } catch (errors) {
+      dispatch({ type: "COLLAB_QUERY_REQUEST_FAILURE", errors: errors })
+    }
+  }
+}
+
+export default queryCollabs
