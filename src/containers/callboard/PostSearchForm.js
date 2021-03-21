06622f38c305c442c5ce4bd78b3c07526b8928ec
Justin McLawhorn <jwmclawhorn@gmail.com>
2021-03-21 12:18:31 -0400

    builds search post form

---
+++ src/containers/callboard/PostSearchForm.js
@@ -0,0 +1,27 @@
+import { useState, useCallback } from 'react'
+import { useDispatch } from 'react-redux'
+import { useAuth0 } from '@auth0/auth0-react'
+import queryPosts from '../../actions/callboard/queryPosts'
+
+const PostSearchForm = () => {
+  const [value, setValue] = useState('')
+  const { getAccessTokenSilently } = useAuth0()
+  const dispatch = useDispatch()
+
+  const handleSubmit = useCallback(
+    async event => {
+      event.preventDefault()
+      const token = await getAccessTokenSilently()
+      dispatch(queryPosts(token, value))
+    }, [dispatch, getAccessTokenSilently, value]
+  )
+
+  return (
+    <form className="m-4 flex-shrink-0" onSubmit={ handleSubmit }>
+      <input type="text" value={value} onChange={event => setValue(event.target.value)} className="bg-gray-200 dark:bg-gray-800 p-1 border-thMagenta border-b-2 outline-none dark:text-white"/>
+      <input type="submit" value="Search" className="m-2 p-1 border-thMagenta bg-transparent border-2 text-thMagenta"/>
+    </form>
+  )
+}
+
+export default PostSearchForm
