4726deabe3847985bfc92fb4adb3444a75b8963c
Justin McLawhorn <jwmclawhorn@gmail.com>
2021-03-21 12:15:11 -0400

    moves Collabs file

--- src/containers/collab/Collabs.js
+++ src/containers/collab/Collabs.js
@@ -1,11 +1,12 @@
 import React, { useEffect } from 'react'
-import CollabCard from '../components/collabs/CollabCard'
+import CollabCard from '../../components/collabs/CollabCard'
 import { useSelector, useDispatch } from 'react-redux'
-import readCollabs from '../actions/collabs/readCollabs'
+import readCollabs from '../../actions/collabs/collabList/readCollabs'
 import { useAuth0 } from '@auth0/auth0-react'
 
 const Collabs = () => {
   const collabs = useSelector(state => state.collab.collabList.list) || []
+  const user = window.localStorage.getItem('user')
   const dispatch = useDispatch()
   const { getAccessTokenSilently } = useAuth0()
   
@@ -17,10 +18,17 @@ const Collabs = () => {
   }, [dispatch, getAccessTokenSilently])
 
   return (
-    <div className="h-screen w-full flex flex-row flex-nowrap bg-gray-200 dark:bg-gray-800">
+    <div className="h-screen w-full flex flex-row flex-nowrap bg-gray-200 dark:bg-gray-800 overflow-auto">
       <div className="p-8">
         <h1 className="text-4xl text-gray-700 dark:text-gray-300 font-body italic cursor-default">View your connected collabs.</h1>
-        { collabs.map(collab => <CollabCard collab={ collab.attributes } />) }
+        { collabs.map(collab => {
+          if (collab.sender.id === parseInt(user)) {
+            return <CollabCard profile={ collab.receiver } key={ collab.id } />
+          } else {
+            return <CollabCard profile={ collab.sender } key={ collab.id } />
+          }
+        })
+      }
       </div>
     </div>
   )
