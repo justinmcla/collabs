1b01e218c4dc8bcfe0400f9e54c97115de3f5311
Justin McLawhorn <jwmclawhorn@gmail.com>
2021-03-21 12:14:39 -0400

    moves callboard component

--- src/containers/callboard/Callboard.js
+++ src/containers/callboard/Callboard.js
@@ -1,10 +1,10 @@
 import React, { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { useAuth0 } from '@auth0/auth0-react'
-import Post from '../components/callboard/Post'
-import queryPosts from '../actions/callboard/queryPosts'
-import PostSearchForm from '../containers/callboard/PostSearchForm'
-import NewPostForm from '../containers/callboard/NewPostForm'
+import Post from '../../components/callboard/Post'
+import queryPosts from '../../actions/callboard/queryPosts'
+import PostSearchForm from './PostSearchForm'
+import NewPostForm from './NewPostForm'
 
 const Callboard = () => {
   const posts = useSelector(state => state.callboard.search.results)
@@ -22,10 +22,10 @@ const Callboard = () => {
     <div className="h-screen w-full flex flex-row flex-nowrap bg-gray-200 dark:bg-gray-800 overflow-auto">
       <div className="p-8">
         <h1 className="text-4xl text-gray-700 dark:text-gray-300 font-body italic">View posts on your callboard.</h1>
-        <br />
-        <NewPostForm />
-        <br />
-        <PostSearchForm />
+        <div className="flex">
+          <NewPostForm />
+          <PostSearchForm />
+        </div>
         <div className="flex flex-wrap">
           { posts.map(post => <Post post={ post } key={ post.id }/>) }
         </div>
