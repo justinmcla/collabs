72c333470dee9390aa09c17afe899a26ebee846c
Justin McLawhorn <jwmclawhorn@gmail.com>
2021-03-21 15:10:37 -0400

    change height styling

--- src/containers/collab/CollabRequests.js
+++ src/containers/collab/CollabRequests.js
@@ -18,7 +18,7 @@ const CollabRequests = () => {
   }, [dispatch, getAccessTokenSilently])
 
   return (
-    <div className="h-screen w-full flex flex-row flex-nowrap bg-gray-200 dark:bg-gray-800 overflow-auto">
+    <div className="w-full flex flex-row flex-nowrap bg-gray-200 dark:bg-gray-800 overflow-auto">
       <div className="p-8">
         <h1 className="text-4xl text-gray-700 dark:text-gray-300 font-body italic cursor-default">View your pending requests.</h1>
         { requests.map(request => {
