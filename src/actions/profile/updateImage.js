41017c85ea7de5aaaecb242c864fea6d764dfe6f
Justin McLawhorn <jwmclawhorn@gmail.com>
2021-03-21 12:11:39 -0400

    builds action to update image

---
+++ src/actions/profile/updateImage.js
@@ -0,0 +1,30 @@
+const updateImage = (image, accessToken) => {
+  return async dispatch => {
+    dispatch({
+      type: "NEW_IMAGE_REQUEST"
+    })
+    try {
+      const endpoint = window.localStorage.getItem('endpoint')
+      const response = await fetch(`${process.env.REACT_APP_BASE_API}/profiles/${endpoint}`, {
+        method: 'PATCH',
+        headers: {
+          Authorization: `Bearer ${accessToken}`
+        },
+        body: image
+      })
+      const json = await response.json()
+      if (json.status === 200) {
+        dispatch({ type: "UPDATE_IMAGE_SUCCESS", image: json.data.image_url })
+      } else {
+        throw new Error('Unable to update image')
+      }
+    } catch (errors) {
+      dispatch({
+        type: "IMAGE_REQUEST_FAILURE",
+        errors: errors
+      })
+    }
+  }
+}
+
+export default updateImage
